138dcbe15c6305667a3fabe789ef9221ad9e417f
afshin paydar <afshinpaydar@gmail.com>
2021-03-27 18:47:02 +0800

    add executeEvents

--- Bank/src/Customer.py
+++ Bank/src/Customer.py
@@ -20,6 +20,26 @@ class Customer:
     def createStub(self):
       self.stub = bank_pb2_grpc.BankingStub(grpc.insecure_channel('localhost:'+str(PORT + self.id)))
       return self.stub
+
+    def executeEvents(self):
+      output = {}
+      interfaces = []
+      stub = self.createStub()
+      for event in self.events:
+        output['id'] = self.id
+        if str(event["interface"]) == "query":
+          response = stub.Query(bank_pb2.BalanceRequest(branchid=self.id))
+          interfaces.append(f"{{ 'interface': 'query', 'result': '{response.status}', 'money': {response.ammount} }}")
+        elif str(event["interface"]) == "withdraw":
+          response = stub.Withdraw(bank_pb2.WithdrawRequest(branchid=self.id, ammount = event["money"]))
+          interfaces.append(f"{{ 'interface': 'withdraw', 'result': '{response.status}' }}")
+        elif str(event["interface"]) == "deposit":
+          response = stub.Deposit(bank_pb2.DepositRequest(branchid=self.id, ammount = event["money"]))
+          interfaces.append(f"{{ 'interface': 'deposit', 'result': '{response.status}' }}")
+        else:
+          interfaces.append("Unkown event")
+      output['recv'] = interfaces
+      return output
   
 if __name__ == '__main__':
   
@@ -30,18 +50,5 @@ if __name__ == '__main__':
     interfaces = []
     if str(item["type"]) == "customer":
       customer = Customer(id=int(item["id"]),events=item["events"])
-      stub = customer.createStub()
-      for event in item["events"]:
-        output['id'] = item['id']
-        if str(event["interface"]) == "query":
-          response = stub.Query(bank_pb2.BalanceRequest(branchid=int(item["id"])))
-          interfaces.append(f"{{ 'interface': 'query', 'result': '{response.status}', 'money': {response.ammount} }}")
-        elif str(event["interface"]) == "withdraw":
-          response = stub.Withdraw(bank_pb2.WithdrawRequest(branchid=int(item["id"]), ammount = event["money"]))
-          interfaces.append(f"{{ 'interface': 'withdraw', 'result': '{response.status}' }}")
-        elif str(event["interface"]) == "deposit":
-          response = stub.Deposit(bank_pb2.DepositRequest(branchid=int(item["id"]), ammount = event["money"]))
-          interfaces.append(f"{{ 'interface': 'deposit', 'result': '{response.status}' }}")
+      print(customer.executeEvents())
 
-      output['recv'] = interfaces
-      print(output)
